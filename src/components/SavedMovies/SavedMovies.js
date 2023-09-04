b5e33f821fa768ce319180c6999580a4393ff99d
Applefa4ry <apple.f4iry@yandex.ru>
2023-09-04 23:06:07 +0300

    Исправила, данные поиска на странице сохраненных фильмов больше не сохраняются в localStorage

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -35,7 +35,6 @@ const SavedMovies = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    localStorage.setItem('lastSearch', event.target[0].value)
     MainApi.searchFilms(event.target[0].value)
       .then(films => {
         setFilms(films)

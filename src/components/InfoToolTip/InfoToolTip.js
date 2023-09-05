43d7883d625a2804f0d696a0bc4b21cdcc18596e
Applefa4ry <apple.f4iry@yandex.ru>
2023-09-05 21:02:22 +0300

    Update InfoToolTip.js

--- src/components/InfoToolTip/InfoToolTip.js
+++ src/components/InfoToolTip/InfoToolTip.js
@@ -8,8 +8,8 @@ function InfoToolTip(props){
   return (
     <div className={`popup ${props.isOpen?"popup_opened":"" }`}> 
       <div className={`popup__container popup__sign-container`}>
-        <img className="popup__sign-picture" src={props.setIsPass?Pass:Fail} alt={props.setIsPass?"Pass":"Fail"} />
-        <h2 className="popup__title">{props.setIsPass?"Всё прошло успешно!":"Что-то пошло не так!\nПопробуйте ещё раз."}</h2>
+        <img className="popup__sign-picture" src={props.isPass?Pass:Fail} alt={props.isPass?"Pass":"Fail"} />
+        <h2 className="popup__title">{props.isPass?"Всё прошло успешно!":"Что-то пошло не так!\nПопробуйте ещё раз."}</h2>
       </div>
       <button onClick={props.onClose} type="button" className="popup__close"></button>
     </div>
